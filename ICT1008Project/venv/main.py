788c04245d2783289dba6c756848129b37e8df1a
derekyjj <55313529+derekyjj@users.noreply.github.com>
2020-03-11 17:04:43 +0800

    Update main.py

--- ICT1008Project/venv/main.py
+++ ICT1008Project/venv/main.py
@@ -1,7 +1,8 @@
 import io
 import sys
 import folium
-from PyQt5 import QtCore, QtGui, QtWidgets, QtWebEngineWidgets
+from PyQt5 import QtWidgets, QtWebEngineWidgets
+from PyQt5.QtWidgets import QLabel
 
 
 class Window(QtWidgets.QMainWindow):
@@ -12,42 +13,57 @@ class Window(QtWidgets.QMainWindow):
     def initWindow(self):
         self.setWindowTitle(self.tr("Punggol Transport"))
         self.setFixedSize(1500, 800)
-        self.buttonUI()
+        self.mainpage()
 
-    def buttonUI(self):
-        shortestPathButton = QtWidgets.QPushButton(self.tr("Find shortest path"))
-        # startLocation = QtGui.QComboBox(self)
-        # finalLocation = QtGui.QComboBox(self)
+    def mainpage(self):
+        # starting location dropbox
+        l1 = QLabel()
+        l1.setText("Start Location:")
+        start = QtWidgets.QComboBox(self)
+        start.addItem("Punggol MRT")
+        start.addItem("Punggol LRT")
 
-        # startLocation.setFixedSize(120, 50)
-        # finalLocation.setFixedSize(120, 50)
-        shortestPathButton.setFixedSize(120, 50)
+        # final location dropbox
+        l2 = QLabel()
+        l2.setText("Final Location:")
+        final = QtWidgets.QComboBox(self)
+        final.addItem("test")
 
-        self.view = QtWebEngineWidgets.QWebEngineView()
-        self.view.setContentsMargins(50, 50, 50, 50)
+        # search button
+        search = QtWidgets.QPushButton(self.tr("Find shortest path"))
+        search.setFixedSize(120, 30)
 
+        # add map widget
+        self.viewmap = QtWebEngineWidgets.QWebEngineView()
+        self.viewmap.setContentsMargins(50, 50, 50, 50)
+
+        # create a central widget in horizontal layout
         central_widget = QtWidgets.QWidget()
         self.setCentralWidget(central_widget)
         lay = QtWidgets.QHBoxLayout(central_widget)
 
+        # button container for vertical layout
         button_container = QtWidgets.QWidget()
         vlay = QtWidgets.QVBoxLayout(button_container)
         vlay.setSpacing(20)
+        vlay.addWidget(l1)
+        vlay.addWidget(start)
+        vlay.addWidget(l2)
+        vlay.addWidget(final)
+        vlay.addWidget(search)
         vlay.addStretch()
-        vlay.addWidget(shortestPathButton)
-        # vlay.addWidget(startLocation)
-        # vlay.addWidget(finalLocation)
-        vlay.addStretch()
-        lay.addWidget(button_container)
-        lay.addWidget(self.view, stretch=1)
 
+        # add button container and map into central widget
+        lay.addWidget(button_container)
+        lay.addWidget(self.viewmap, stretch=0)
+
+        # create the map
         m = folium.Map(
             location=[1.3984, 103.9072], zoom_start=18
         )
-
         data = io.BytesIO()
         m.save(data, close_file=False)
-        self.view.setHtml(data.getvalue().decode())
+        self.viewmap.setHtml(data.getvalue().decode())
 
 
 if __name__ == "__main__":
