a21df1094f70503b725007224e849026827680e8
swipaaar <ongtiongpoh@hotmail.com>
2020-03-23 17:58:18 +0800

    added dataset stuff

--- ICT1008Project/venv/Dataset.py
+++ ICT1008Project/venv/Dataset.py
@@ -1,11 +1,8 @@
 import sys
 
 class Vertex:
-    def __init__(self, nid, name, address, postal, lon, lat):
+    def __init__(self, nid, lon, lat):
         self.nid = nid
-        self.name = name
-        self.address = address
-        self.postal = postal
         self.lon = lon
         self.lat = lat
         self.visited = False
@@ -17,30 +14,11 @@ class Vertex:
     def getNID(self):
         return self.nid
 
-    def getName(self):
-        return self.name
-
-    def getAddress(self):
-        return self.address
-
-    def getPostal(self):
-        return self.postal
-
     def getLongitude(self):
         return self.lon
 
     def getLatitude(self):
         return self.lat
-
-    def addNeighbour(self, neighbour, weight=0):
-        self.neighbour[neighbour] = weight
-
-    def getNeighbour(self):
-        return self.neighbour.keys()
-
-    def printNeighbour(self):
-        for x in self.neighbour:
-            print("key=" + str(x.getNID()) + " value=" + str(self.neighbour.get(x)))
 
 class Graph:
     def __init__(self):
@@ -53,28 +31,40 @@ class Graph:
     def getVertices(self):
         return list(self.data.keys())
 
-    def getEdges(self, vertex):
-        return self.data
+    def getEdges(self, nid):
+        return self.data[nid]
 
-    def addVertex(self, vertex):
+    def getVertex(self, nid):
+        for key in self.data.keys():
+            if key == nid:
+                return key
+        return None
+
+    def addVertex(self, nid):
         # if key not in dict
-        if vertex not in self.data:
-            self.data[vertex] = []
+        if nid not in self.data:
+            self.data[nid] = {}
 
-    def addEdge(self, fromVertex, toVertex, weight=0):
-        if fromVertex not in self.data:
-            self.addVertex(fromVertex)
-        if toVertex not in self.data:
-            self.addVertex(toVertex)
+    def addHDBEdge(self, fromNID, toNID, weight=0):
+        if fromNID not in self.data:
+            self.addVertex(fromNID)
+        if toNID not in self.data:
+            self.addVertex(toNID)
 
         # Both ways
-        fromVertex.addNeighbour(toVertex, weight)
-        toVertex.addNeighbour(fromVertex, weight)
-        self.data[fromVertex] = (toVertex, weight)
-        self.data[toVertex] = (fromVertex, weight)
-        # self.data[fromVertex].addNeighbour(toVertex, weight)
-        # self.data[toVertex].addNeighbour(fromVertex, weight)
+        self.data[fromNID][toNID] = weight
+        self.data[toNID][fromNID] = weight
+
+    def addBusEdge(self, fromNID, toNID, BusSvc, direction, weight=0):
+        if fromNID not in self.data:
+            self.addVertex(fromNID)
+        if toNID not in self.data:
+            self.addVertex(toNID)
+
+        # Both ways
+        self.data[fromNID][(toNID, (BusSvc, direction))] = weight
 
     def printGraph(self):
         for x in self.data:
-            print("key= " + str(x.getNID()) + " value= " + str(self.data.get(x)[0].getNID()))
+            print(str(x) + ": ", end="")
+            print(str(self.data.get(x)))
---
+++ ICT1008Project/venv/Populate.py
@@ -0,0 +1,105 @@
+import json
+from math import cos, asin, sqrt
+
+def populateHDB(mapGraph, infoDict):
+
+    with open('Punggol_HDB.json') as f:
+        data = json.load(f)
+    f.close
+    for key in data.keys():
+        mapGraph.addVertex(data[key][0]['POSTAL'])
+        infoDict[data[key][0]['POSTAL']] = {}
+        infoDict[data[key][0]['POSTAL']]['Name'] = data[key][0]['NAME']
+        infoDict[data[key][0]['POSTAL']]['Address'] = data[key][0]['ADD']
+        infoDict[data[key][0]['POSTAL']]['Latitude'] = data[key][0]['LATITUDE']
+        infoDict[data[key][0]['POSTAL']]['Longitude'] = data[key][0]['LONGTITUDE']
+
+    return mapGraph, infoDict
+
+def addHDBtoBusStop(mapGraph, infoDict):
+
+    for key in infoDict:
+        # if is postal code
+        if len(key) == 6:
+            distarr = []
+            key2arr = []
+            for key2 in infoDict:
+                # check all bus stop
+                if len(key2) == 5:
+                    # find closest bus stops to the hdb
+                    dist = distance(infoDict[key]['Latitude'], infoDict[key]['Longitude'], infoDict[key2]['Latitude'],
+                                    infoDict[key2]['Longitude'])
+                    distarr.append(dist)
+                    key2arr.append(key2)
+            mapGraph.addHDBEdge(key, key2arr[distarr.index(min(distarr))], min(distarr))
+
+    return mapGraph
+
+def populateBus(mapGraph, infoDict):
+
+    with open('BusStop.json') as f:
+        data = json.load(f)
+    f.close
+    # Add busstops
+    for x in data:
+        mapGraph.addVertex(data[x]['BusStopCode'])
+        if 'Latitude' in data[x]:
+            infoDict[data[x]['BusStopCode']] = {}
+            infoDict[data[x]['BusStopCode']]['Description'] = data[x]['Description']
+            infoDict[data[x]['BusStopCode']]['RoadName'] = data[x]['RoadName']
+            infoDict[data[x]['BusStopCode']]['Latitude'] = data[x]['Latitude']
+            infoDict[data[x]['BusStopCode']]['Longitude'] = data[x]['Longitude']
+
+    return mapGraph, infoDict
+
+def addBusRoute(mapGraph):
+    with open('Bus_Route.json') as f:
+        route = json.load(f)
+    f.close
+    # Add bus routes
+    for y in range(len(route)):
+        if "LATITUDE" in route[y] and "LATITUDE" in route[y-1]:
+            if route[y]['ServiceNo'] == route[y-1]['ServiceNo'] and route[y]['Direction'] == route[y-1]['Direction']:
+                dist = distance(route[y-1]['LATITUDE'], route[y-1]['LONGTITUDE'], route[y]['LATITUDE'],
+                                route[y]['LONGTITUDE'])
+                mapGraph.addBusEdge(route[y-1]['BusStopCode'], route[y]['BusStopCode'], route[y-1]['ServiceNo'],
+                                    route[y-1]['Direction'], dist)
+
+    return mapGraph
+
+def populateLRT(mapGraph, infoDict):
+    with open('LRT.json') as f:
+        data = json.load(f)
+    f.close
+    # Add LRT
+    for x in data:
+        mapGraph.addVertex(x)
+        infoDict[x] = {}
+        infoDict[x]['Name'] = data[x][0]['NAME']
+        infoDict[x]['Latitude'] = data[x][0]['LATITUDE']
+        infoDict[x]['Longitude'] = data[x][0]['LONGTITUDE']
+
+    return mapGraph, infoDict
+
+def addLRTtoBusStops(mapGraph, infoDict):
+    for key in infoDict:
+        # if is LRT
+        if int(key) < 15:
+            for key2 in infoDict:
+                # check all bus stop and HDB
+                if len(key2) == 5 or len(key2) == 6:
+                    # find closest bus stops to the hdb
+                    dist = distance(infoDict[key]['Latitude'], infoDict[key]['Longitude'], infoDict[key2]['Latitude'],
+                                    infoDict[key2]['Longitude'])
+                    mapGraph.addHDBEdge(key, key2, dist)
+    return mapGraph
+
+def distance(lat1, lon1, lat2, lon2):
+    # Haversine formula, to calculate the distance between given latitude and longitude
+    lat1 = float(lat1)
+    lon1 = float(lon1)
+    lat2 = float(lat2)
+    lon2 = float(lon2)
+    p = 0.017453292519943295
+    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
+    return 12742 * asin(sqrt(a))
