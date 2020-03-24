829ad8ca22802b09b1dfb5cb58421ed704a49a96
Terence-Fong <57536377+Terence-Fong@users.noreply.github.com>
2020-03-24 18:12:00 +0800

    Algo update

---
+++ ICT1008Project/venv/AlgoTest.py
@@ -0,0 +1,133 @@
+from onemapsg import OneMapClient
+from Dataset import Graph
+
+import json
+import heapq
+
+def get_jsonLRT():
+    results = json.loads(open("LRT.json").read())
+    return results
+
+def get_jsonHDB():
+    results = json.loads(open("Punggol_HDB.json").read())
+    return results
+
+def get_jsonRoutes():
+    #LRT.json to be replaced with routes json
+    results = json.loads(open("LRT.json").read())
+    return results
+
+def get_jsonStops():
+    #LRT.json to be replaced by stops json
+    results = json.load(open("LRT.json").read())
+    return results
+
+#Need json for every bus stop and lrt
+#Need json for all the bus routes and lrt routes
+
+lrt = get_jsonLRT()
+hdb = get_jsonHDB()
+routes = get_jsonRoutes()
+stops = get_jsonStops()
+
+#converting flat lists into dictionaries of Descriptions THE KEY IS DESCRIPTION
+stop_map = {stop['Description']: stop for stop in stops}
+#converting flat lists into dictionaries of Bus Stop Codes THE KEY IS BUS STOP CODE
+code_map = {stop['BusStopCode']: stop for stop in stops}
+
+
+route_map = {}
+
+def calculate_route(start, end): #1
+
+    if start and end:
+            routes_map = {}
+
+            for route in routes:
+                #start to create each node in the route
+                key = (route["ServiceNo"], route["Direction"])
+                if key not in routes_map:
+                    routes_map[key] = []
+                routes_map[key] += [route]
+
+            # call the djikstra algorithm and get back the total distance and the shortest route
+            distance, path = find_shortest_route(stop_map[start]["BusStopCode"],
+                                                                              stop_map[end]["BusStopCode"], routes_map)
+
+            #print out the route
+            for code, service in path:
+                street_name = str(code_map[code]["Description"])
+
+                print(street_name)
+
+            #store all the data into the strings
+            no_of_stops = "The bus trip took " + str(len(path)) + " stops"
+
+            print(no_of_stops)
+
+    else:
+        print("Error", "Please enter something")
+
+
+def find_shortest_route(start,end,routes_map):
+
+
+    """
+    CREATING MAP OF NODES FOR ALL BUS STATIONS IN SINGAPORE
+    """
+
+    graph = {}
+    for service, path in routes_map.items(): #for each bus service
+        for route_index in range(len(path) - 1): #for each route
+            key = path[route_index]["BusStopCode"]
+            if key not in graph:
+                graph[key] = {}
+            curr_route_stop = path[route_index] #get current node
+            next_route_stop = path[route_index + 1] #get adjacent node
+            curr_distance = curr_route_stop["Distance"] or 0
+            next_distance = next_route_stop["Distance"] or curr_distance
+            distance = next_distance - curr_distance    #calculate the distance from the next node to current
+            assert distance >= 0, (curr_route_stop, next_route_stop) #error checking (distance cant be less than 0)
+            curr_code = curr_route_stop["BusStopCode"]
+            next_code = next_route_stop["BusStopCode"]
+            graph[curr_code][(next_code, service)] = distance #store current node and adjacent node
+
+
+    """
+    START OF DIJKSTRA ALGORITHM
+    """
+    seen = set()
+    # maintain a queue of paths
+    queue = []
+    # push the first path into the queue
+    heapq.heappush(queue, (0, 0, 0, [(start, None)]))
+    while queue:
+        # get the first path from the queue
+        (curr_cost, curr_distance, curr_transfers, path) = heapq.heappop(queue)
+
+        # get the last node from the path
+        (node, curr_service) = path[-1]
+
+        # path found
+        if node == end:
+            return (curr_distance, path)
+
+        if (node, curr_service) in seen:
+            continue
+
+        seen.add((node, curr_service))
+        # enumerate all adjacent nodes, construct a new path and push it into the queue
+        for (adjacent, service), distance in graph.get(node, {}).items():
+            new_path = list(path)
+            new_path.append((adjacent, service))
+            new_distance = curr_distance + distance
+            new_cost = distance + curr_cost
+            new_transfers = curr_transfers
+            if curr_service != service:
+                new_cost += 10
+                new_transfers += 1
+            new_cost += 0.5
+
+            heapq.heappush(queue, (new_cost, new_distance, new_transfers, new_path))
+
+
--- ICT1008Project/venv/OneMapAPI.py
+++ ICT1008Project/venv/OneMapAPI.py
@@ -1,24 +1,73 @@
 from onemapsg import OneMapClient
-import json
+# for authentication
+Client = OneMapClient("ibudinsa@gmail.com", "markred29125")
 
-Client = OneMapClient("ibudinsa@gmail.com", "markred29125") # for authentication
+# search for address
+print('Enter your starting location to search:')
+inputLocation = input()
 
-print('Enter your Location to search:')
-inputLocation = input() # search for address
+#inserting the dictionary into store
+storeDicOfSearchStart = (Client.search(inputLocation)['results'][0])
+startLocation = Client.search(inputLocation)['results'][0]['SEARCHVAL']
 
-storeDicOfSearch = Client.search(inputLocation) #inserting the dictionary into store
+# search for destination
+print("Enter your location for destination: ")
+inputDestination = input()
 
-totalpage = storeDicOfSearch["totalNumPages"] # extracting value of key "totalNumPages" from dic
-searchResult = storeDicOfSearch['results'] # store list of dictionary search values
+#inserting the dictionary into store
+storeDicOfSearchDestination = (Client.search(inputDestination)['results'][0])
+endlocation = Client.search(inputDestination)['results'][0]['SEARCHVAL']
+coordinatesOfStart = [storeDicOfSearchStart['LATITUDE'],storeDicOfSearchStart['LONGITUDE']]
+coordinatesOfDestination = [storeDicOfSearchDestination['LATITUDE'],storeDicOfSearchDestination['LONGITUDE']]
+x = Client.get_public_transport_route(coordinatesOfStart,coordinatesOfDestination,20200430,152030,"BUS")
 
-counter =0
-for i in range(totalpage): # Extracting individual list data from each location
-    storeDicOfSearch2 = Client.search(inputLocation,True,True,i+1)
-    searchResult2 = storeDicOfSearch2['results']
-    for x in range(len(searchResult)): # printing each building address individually
-        counter +=1
-        print("Search Result No", counter ," : ",searchResult[x])
+routesStore = (((((x['plan'])['itineraries'])[0])['legs'])) # this enters the legs list, the 0 means option 1
 
-print ("Total number of Results: ", counter)
-x = OneMapClient.get_public_transport_route([1.378509975,103.84980279999999],[1.3694385619999998,103.8493344],20200430,152030,"BUS")
-print (x)
+print("Start location: ",startLocation,"End location: ",endlocation)
+print (routesStore)
+for i in range(len(routesStore)-1):
+    if routesStore[i]['mode'] == 'WALK':
+        distanceOf1 = int(routesStore[i]['distance']) # 1st tab of the search
+        nameOfLocation1 = routesStore[i]['to']['name']
+        timeTaken1 = float((routesStore[i]['duration']) / 60)
+        print("Walk for ", distanceOf1, "meters to ", nameOfLocation1, " this will take: ", timeTaken1, " Mins")
+    elif routesStore[i]['mode'] == "BUS":
+        distanceOf1 = int(routesStore[i]['distance']) # 1st tab of the search
+        nameOfLocation1 = routesStore[i]['to']['name']
+        timeTaken1 = float((routesStore[i]['duration']) / 60)
+        startBusStopNo = routesStore[i]['from']['stopCode']
+        endBusStopNo = routesStore[i]['to']['stopCode']
+        bustakenNo = routesStore[i]['route']
+        busstopName = routesStore[i]['from']['name']
+        # print("Take the Bus for", distanceOf1, "meters to ", nameOfLocation1, "(Bus Stop Number):", originBusStopNo,"take bus number:",bustakenNo,
+        #       " this will take a total time of:", timeTaken1, " Mins")
+        print("Take bus:",bustakenNo,"from bus stop :",busstopName,", Bus Stop number",startBusStopNo,"to",nameOfLocation1,", Bus stop number",endBusStopNo,".","This will take a total of:",timeTaken1,"mins.")
+    else:
+        print ("error")
+
+# for i in range(len(routesStore)-1):
+#     distanceOf1 = int(routesStore[i]['distance']) # 1st tab of the search
+#     nameOfLocation1 = routesStore[i]['to']['name']
+#     timeTaken1 = float((routesStore[i]['duration']) / 60)
+#     try:
+#         originBusStopNo = routesStore[i]['to']['stopCode']
+#     except:
+#         print("Walk for ", distanceOf1, "meters to ", nameOfLocation1," this will take: ", timeTaken1, " Mins")
+#         continue
+#     print("Walk for ", distanceOf1,"meters to ",nameOfLocation1," Bus Stop Number: ",originBusStopNo," this will take: ",timeTaken1," Mins")
+
+
+
+# storeDicOfSearch = Client.search(inputLocation) #inserting the dictionary into store
+# totalpage = storeDicOfSearch["totalNumPages"] # extracting value of key "totalNumPages" from dic
+# searchResult = storeDicOfSearch['results'] # store list of dictionary search values
+
+# counter =0
+# for i in range(totalpage): # Extracting individual list data from each location
+#    storeDicOfSearch2 = Client.search(inputLocation,True,True,i+1)
+#    searchResult2 = storeDicOfSearch2['results']
+#    for x in range(len(searchResult)): # printing each building address individually
+#        counter +=1
+#        print("Search Result No", counter ," : ",searchResult[x])
+
+# print ("Total number of Results: ", counter)
